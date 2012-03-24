af41b0cac311063cb8a475897580bd9573a7dd13
Christian Moscardi <clm2183@columbia.edu>
2012-03-24 18:08:58 -0400

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Litterbox::Application.config.secret_token = '7434d3597534248914805f419547005f36de5cbc1a3d886f2100e9a1d17d072fa63ce5c4cc86b72da9d09369cdb35f172e4d55a32a232c341f5ef3fbbd175687'
