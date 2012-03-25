aadddeabd382bbf86ad26fc1b9bda099ca5ed220
Christian Moscardi <clm2183@columbia.edu>
2012-03-25 13:11:20 -0400

    changing configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,9 @@ Litterbox::Application.configure do
 
   #action mailer
   config.action_mailer.default_url_options = { :host => 'heroku.com' }
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
 end
