9ee6da1664b7ac519fe11f697186787c01b698f3
Christian Moscardi <clm2183@columbia.edu>
2012-03-24 20:52:56 -0400

    of course it actually wasnt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_cat!
+  #before_filter :authenticate_cat!
 
   def index
     if cat_signed_in? 
       cat= Cat.find(params[:id])
       redirect_to :controller => 'cats', :action => 'show'
+    end
+
   end
 end
